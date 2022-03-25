8b779348ee8bd9c26c2f3f52e6d4f395eb6a7fd2
Yamanki Santander <syamanki@gmail.com>
2022-03-24 22:25:00 -0600

    Code first version. Naturals numbers sqrt

---
+++ list_comprehensions_challenge.py
@@ -0,0 +1,11 @@
+def run():
+    naturals_sqr=[]
+
+    for i in range(1,101):
+        sqr=i**2
+        naturals_sqr.append(sqr)
+    
+    print(naturals_sqr)
+
+if __name__ == "__main__":
+    run()
