f2f7b14ff4e223978838e478c30fc0b13f88e7e2
Yamanki Santander <syamanki@gmail.com>
2022-03-25 12:17:26 -0600

    high order reduce function

--- high_order_ex.py
+++ high_order_ex.py
@@ -6,6 +6,19 @@ def run():
     sqr_map= list(map(lambda x: x**2, my_list))
     print(sqr, " vs", sqr_map)
 
+    #Miltiply all 
+    my_list = [2,2,2,2,2]
+    all_multiplied = 1
+
+    for i in my_list:
+        all_multiplied= all_multiplied * i
+
+    ### by applying reduce
+    from functools import reduce
+
+    all_multiplied_red = reduce(lambda a,b: a* b, my_list)
+    print(all_multiplied, 'vs', all_multiplied_red)
+
 
 if __name__ == '__main__':
     run()
