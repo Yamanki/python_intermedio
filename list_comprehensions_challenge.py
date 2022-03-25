ddaf3f39f0be2f7f56405ac079a1281bfa551456
Yamanki Santander <syamanki@gmail.com>
2022-03-24 22:42:21 -0600

    modified code based on the lists of comprehension method

--- list_comprehensions_challenge.py
+++ list_comprehensions_challenge.py
@@ -1,10 +1,5 @@
 def run():
-    naturals_sqr=[]
-
-    for i in range(1,101):
-        if i %3 !=0:
-            sqr=i**2
-            naturals_sqr.append(sqr)
+    naturals_sqr=[i**2 for i in range(1,101) if i % 3 !=0]
     
     print(naturals_sqr)
 
